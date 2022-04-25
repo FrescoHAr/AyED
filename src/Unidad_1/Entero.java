30afbac334bcc606e5b8744ad58af48b22eb8576
Hernan Fresco <herny.12@gmail.com>
2022-04-24 23:35:01 -0300

    criba, menorDivisor, factorizacion y mejora en resto y cociente

--- src/Unidad_1/Entero.java
+++ src/Unidad_1/Entero.java
@@ -1,4 +1,6 @@
 package Unidad_1;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Entero {
 	private int valor;
@@ -28,19 +30,22 @@ public class Entero {
 		int b= divisor;
 		dividendo = Math.abs(dividendo);
 		divisor  = Math.abs(divisor);
-		int iteraciones =0;
+		int cociente =0;
 		while(dividendo>=divisor) {
 			dividendo-=divisor;
-			iteraciones++;
+			cociente++;
 		}
 		if(a*b<0) {
-			if(a<0)
-				return -iteraciones-1;
+			if(-(cociente+1)*b<a)
+				return -(cociente+1);
 			else
-				return -iteraciones;
+				return-cociente;
 		}
 		else
-			return iteraciones;
+			if((cociente+1)*b<a)
+				return cociente+1;
+			else 
+				return cociente;
 		
 	}
 	
@@ -54,14 +59,18 @@ public class Entero {
 	
 	
 	public int resto(int dividendo, int divisor) {
-		int a= Math.abs(dividendo);
+		return dividendo -divisor*cociente(dividendo,divisor); //Por propiedades de la division entera
+		
+		// Tambien se podria haber calculado de manera independiente 
+		/*int a= Math.abs(dividendo);
 		int b= Math.abs(divisor);
 		while(a>=b)
 			a-=b;
 		if(dividendo<0&&divisor>0)
-			return divisor-a;
+			return divisor-a; // resto por ecceso ya que en negativos d = d+1 para que se pueda complir qxd <= D< (q+1)xd
 		else 
 			return a;
+		*/
 		
 	}
 	
@@ -160,5 +169,191 @@ public class Entero {
 	 */
 	public int mcm(Entero e) {
 		return this.mcm(this.getValor(), e.getValor());
+	}
+	
+	
+	
+	public void trianguloPrueba(int pisos) {
+		if(pisos>0) {
+			System.out.println("*");
+		}
+		for(int i=2;i<pisos;i++) {
+			String vacios = new String(new char[i-2]).replace("\0", " ");
+			System.out.println("*"+vacios+"*");
+		}
+		String estrellas = new String(new char[pisos]).replace("\0", "*");
+		System.out.println(estrellas);
+		
+	}
+	
+	
+	/** factorizacion(valor) ->HasMap<base,exponente>
+	 * @param valor - int
+	 * @param base - Integer
+	 * @param exponente - Integer
+	 * Devuelve la factorizacion en numeros primos del parametro <valor>, en un HasMap que contiene 
+	 * {base=exponente} donde 
+	 * 	base es un factor primo y
+	 * 	exponente es la cantidad de veces que aparece 
+	 */
+	public HashMap<Integer,Integer> factorizacion(int numero) {
+		HashMap<Integer,Integer> factores = new HashMap<Integer,Integer>();
+		while(numero!=1) {
+			int divisor=2;
+			if(numero%2!=0) {
+				divisor=3;
+
+				while(numero%divisor!=0) {
+					divisor+=2;
+				}
+
+			}
+			if(factores.containsKey(divisor)) {
+				factores.put(divisor,factores.get(divisor)+1);
+			}
+			else
+				factores.put(divisor, 1);
+			numero /=divisor;
+				
+		}
+		return factores;
+		
+	}
+	
+	/**
+	 * factorizacion() -> HasMap<base,exponente>
+	 * @param base - Integer
+	 * @param exponente - Integer
+	 * Devuelve la factorizacion en numeros primos del parametro almacenado con setValor(), en un HasMap que contiene 
+	 * {base=exponente} donde 
+	 * 	base es un factor primo y
+	 * 	exponente es la cantidad de veces que aparece 
+	 */
+	public HashMap<Integer,Integer> factorizacion(){
+		return this.factorizacion(this.getValor());
+	}
+	
+	/**
+	 * factorizacion(e) -> HasMap<base,exponente>
+	 * @param e - Entero()
+	 * @param base - Integer
+	 * @param exponente - Integer
+	 * Devuelve la factorizacion en numeros primos del parametro almacenado con e.gtValor(), en un HasMap que contiene 
+	 * {base=exponente} donde 
+	 * 	base es un factor primo y
+	 * 	exponente es la cantidad de veces que aparece 
+	 */
+	public HashMap<Integer,Integer> factorizacion(Entero e){
+		return this.factorizacion(e.getValor());
+	}
+	
+	
+	
+	
+	/**
+	 * Retorna la criba Erastotenes hasta un limite pasado por parametro
+	 * en un array de enteros, donde los numeros que son primos conservan su valor y los compuestos fueron reeemplazados por 0
+	 * 
+	 * criba(int inicio, int fin) -> int[] 
+	 * @param inicio -> int
+	 * @param fin -> int 
+	 * 
+	 * 
+	 */
+	public int[] criba(int inicio,int fin) {
+		int [] listado = new int[fin-inicio+1];
+		int multiplos;
+		for(int i=0;i<=fin-inicio;i++) {
+			listado[i]=i+inicio;
+		}
+		System.out.println();
+		int indice = 0;
+		while(indice<fin/2) {
+			if(listado[indice]!=0) {
+				if(esPrimo(listado[indice])) {
+					multiplos = listado[indice];
+				}
+				
+				else {
+					if(listado[indice]>1) {
+						multiplos = menorDivisor(listado[indice]);	
+					}
+					else
+						multiplos = inicio+fin;
+					listado[indice]=0;
+					
+					
+				}
+				for(int i=indice+multiplos;i<=fin-inicio;i=i+multiplos) {
+					listado[i]=0;
+				}
+			}
+			indice++;
+		}
+		return listado;
+	}
+	
+	
+	/**
+	 * Retorna la criba Erastotenes hasta un limite pasado por parametro
+	 * en un array de enteros, donde los numeros que son primos conservan su valor y los compuestos fueron reeemplazados por 0
+	 * 
+	 * criba(int inicio, int fin) -> int[] 
+	 * @param fin -> int 
+	 * 
+	 * 
+	 */
+	public int[] criba(int fin) {
+		return criba(0,fin);
+	}
+	
+	/**
+	 * Retorna la criba Erastotenes hasta un limite igual al valor almacenado el objeto que llamé a esta funcion
+	 * en un array de enteros, donde los numeros que son primos conservan su valor y los compuestos fueron reeemplazados por 0
+	 * 
+	 * criba(int inicio, int fin) -> int[] 
+	 */
+	public int[] criba() {
+		return criba(0,this.getValor());
+	}
+	
+	
+	/**Limpia el array criba quitandole los valores compuestos
+	 * 
+	 * @param criba es una representacion de la criba de erastotenes donde los valores compuestos estan reemplazados por 0
+	 * @return Un ArrayList de Integer con los primos que contenga el array criba
+	 */
+	public ArrayList<Integer> limpiarCriba(int[] criba){
+		ArrayList<Integer> listaCriba = new ArrayList<>();
+		for(int i=0;i<criba.length;i++) {
+			if(criba[i]!=0)
+				listaCriba.add(criba[i]);
+		}
+		return listaCriba;
+	}
+	
+	/**
+	 * Genera una  lista de primos entre dos valores pasados por parametros
+	 * @param inicio - int
+	 * @param fin - int 
+	 * @return ArrayList Integer con todos los valores primos que esten entre i 	nicio y fin
+	 */
+	public ArrayList<Integer> cribaLimpia(int inicio, int fin) {
+		return this.limpiarCriba(this.criba(inicio,fin));
+	}
+	
+	
+	public int menorDivisor(int valor) {
+		if(resto(valor,2)==0)
+			return 2;
+		else {
+			int divisor=3;
+			while(divisor*divisor<valor&&resto(valor,divisor)!=0)
+				divisor+=2;
+			if(resto(valor,divisor)==0)
+				return divisor;
+			else
+				return valor;
+		}
 	}
 }
