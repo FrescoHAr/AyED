3b2b87481ae0c4f188ca680756c0e2d53d6c4ddd
Hernan Fresco <herny.12@gmail.com>
2022-05-09 16:53:32 -0300

    Unidad 1

---
+++ Unidad 1/src/Test/Test.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import ar.edu.unsa.Unidad1.Entero;
+
+/**
+ *
+ * @author Hernán Fresco
+ *
+ * https://github.com/frescoh
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("a".compareTo("b"));
+		System.out.println("a".equals("b"));
+	
+	
+	}
+
+}
